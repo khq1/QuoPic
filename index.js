6dc72aa95ad2f3467724b43108209e62fe7baa49
khq1 <74719753+khq1@users.noreply.github.com>
2021-06-10 12:46:19 +0200

    commit

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-
 window.onload = getimageandtext
-
-
-function getimageandtext(){fetch ('https://api.quotable.io/random') .then(response => response.json())
-.then( data =>{
+function getimageandtext(){fetch ('https://api.quotable.io/random') .then(response => response.json()) 
+.then( data =>{ 
 document.getElementById("text_box").innerHTML = `${data.content} <br><br>${data.author}`
 
-var linktest = "https://picsum.photos/400/600?random=1";
+var linktest = "https://picsum.photos/360/550?random=1";
 var random = Math.floor(Math.random() *9999);
 const string1 = "https://picsum.photos/";
-var imageheight = document.body.clientHeight;
+var imageheight   = window.innerHeight;
 var string2 = "/";
-var imagewidth = document.body.clientWidth;
+var imagewidth   = window.innerWidth;
 const string3 = "?random=";
-linktorandomIMGapi = string1+imageheight+string2+imagewidth+string3+random; document.getElementById("tlo").style.background = 
-"url("+linktorandomIMGapi+")";
+linktorandomIMGapi = string1+imagewidth+string2+imageheight+string3+random; document.getElementById("tlo").style.background = 
+"url("+linktorandomIMGapi+")"; 
 
 console.log(random)
 console.log(linktorandomIMGapi)
 console.log(linktest)
 speak('hello world!')
+
 });
 }
 
