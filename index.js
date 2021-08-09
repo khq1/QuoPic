e5314dbd8abc0c0cfae1a91ce50f9fa047e6415c
khq1 <74719753+khq1@users.noreply.github.com>
2021-08-09 04:47:25 +0200

    alfa

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ window.onload = getimageandtext
 function getimageandtext(){fetch ('https://api.quotable.io/random') .then(response => response.json()) 
 .then( data =>{ 
 document.getElementById("text_box").innerHTML = `${data.content} <br><br>${data.author}`
+var msg = new SpeechSynthesisUtterance();
+var voices = window.speechSynthesis.getVoices();
+msg.voice = voices[10]; // Note: some voices don't support altering params
+msg.voiceURI = 'native';
+msg.volume = 1; // 0 to 1
+msg.rate = 0.9; // 0.1 to 10
+msg.pitch = 0.5; //0 to 2
+msg.text = `${data.content} ${data.author}`;
+msg.lang = 'en-EN';
+
+msg.onend = function(e) {
+  console.log('Finished in ' + event.elapsedTime + ' seconds.');
+};
+
+speechSynthesis.speak(msg);
+
 
 var linktest = "https://picsum.photos/360/550?random=1";
 var random = Math.floor(Math.random() *9999);
@@ -10,13 +26,14 @@ var imageheight   = window.innerHeight;
 var string2 = "/";
 var imagewidth   = window.innerWidth;
 const string3 = "?random=";
-linktorandomIMGapi = string1+imagewidth+string2+imageheight+string3+random; document.getElementById("tlo").style.background = 
+  linktorandomIMGapi = string1 + imagewidth + string2 + imageheight + string3 + random;
+  document.getElementById("tlo").style.background =
 "url("+linktorandomIMGapi+")"; 
 
 console.log(random)
 console.log(linktorandomIMGapi)
 console.log(linktest)
-speak('hello world!')
+
 
 });
 }
