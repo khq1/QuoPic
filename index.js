19ac69dcadd5b5d48b54ae3b002bb72ef69e7d12
khq1 <74719753+khq1@users.noreply.github.com>
2021-12-07 00:40:28 +0100

    gitignore

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 window.onload = getimageandtext;
+
+
 function getimageandtext() { //get quotes
   fetch("https://api.quotable.io/random")
     .then((response) => response.json())
@@ -6,15 +8,16 @@ function getimageandtext() { //get quotes
       document.getElementById(
         "text_box"
       ).innerHTML = `<p>${data.content}<br><br><i>${data.author}</p>`;
+      console.log(JSON.parse(JSON.stringify(data)))
 
 
       var msg = new SpeechSynthesisUtterance(); //define speech
       var voices = window.speechSynthesis.getVoices();
       msg.voice = voices[10]; // Note: some voices don't support altering params
-      msg.voiceURI = "Google US English";
+      msg.voiceURI = "Google EN English";
       msg.volume = 1; // 0 to 1
       msg.rate = 1; // 0.1 to 10
-      msg.pitch = 0.5; //0 to 2
+      msg.pitch = 0.6; //0 to 2
       msg.text = `${data.content} ${data.author}`;
       msg.lang = "en-EN";
 
@@ -30,11 +33,11 @@ function getimageandtext() { //get quotes
         var imagewidth = window.innerWidth;
         const string3 = "?random=";
 
-        var linktorandomIMGapi =
+        var LinktoRandomApi =
           string1 + imagewidth + string2 + imageheight + string3 + random; //custom link
 
         document.getElementById("background").style.background =
-          "url(" + linktorandomIMGapi + ")";
+          "url(" + LinktoRandomApi + ")";
       }//get background image with custom link
          getBGimage();
       
